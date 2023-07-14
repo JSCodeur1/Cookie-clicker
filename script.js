b3affb93fbee77c87b4831353399751e33cd2221
JSCodeur1 <138124682+JSCodeur1@users.noreply.github.com>
2023-07-14 11:40:34 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+(function (d) {
+    var tourner = 0;
+    var gagne = 0;
+    var compteur = 0;
+    var cpc = 1;
+    const texte = d.querySelector('.score');
+    const bouton = d.querySelector('.cookie');
+    const plus_25 = d.querySelector('.plus-25');
+
+    function ajouter(){
+        if (!gagne){
+            compteur += cpc;
+            texte.innerHTML = "Score : " + compteur + "<br> Cookies par clic : " + cpc;
+            if (compteur >= 10){
+                alert("Tu as gagné !!!");
+                bouton.style.color = "grey";
+                plus_25.style.color = "grey";
+                texte.style.color = "grey";
+                gagne = 1;
+            };
+        } else {
+            alert("Tu as deja gagne !");
+        }
+        
+    };
+
+    function acheter(nbr, cout){
+        if (!gagne){
+            if (compteur >= cout){
+                compteur -= cout;
+            cpc += nbr;
+                texte.innerHTML = "Score : " + compteur + "<br> Cookies par clic : " + cpc;
+            };
+        } else {
+            alert("Tu as deja gagné !");
+        }
+        
+    };
+
+    bouton.addEventListener('click', function(){
+        ajouter();
+    });
+
+    plus_25.addEventListener('click', function(){acheter(1,25);});
+    
+})(document);
